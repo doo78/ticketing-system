8c8460c22c50f0f19694ac86f605fcc4b7a03663
Saleh AlSubaie <subaeisaleh@gmail.com>
2025-02-25 12:52:16 +0000

    Update settings.py

--- my_library/settings.py
+++ my_library/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  
 ]
-=======
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
