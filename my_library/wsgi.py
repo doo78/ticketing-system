8690b8a20a2b7e16040797f2443075c06cbf503d
Saleh AlSubaie <subaeisaleh@gmail.com>
2025-03-31 10:56:23 +0300

    Update wsgi.py

--- my_library/wsgi.py
+++ my_library/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-path = '/home/aborashood/my_library'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'my_library.settings'
-
-# then:
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_library.settings')
+
+application = get_wsgi_application()
